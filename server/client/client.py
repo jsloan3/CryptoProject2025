e47bc178251ff3120f07d7dbbbf9e2d68737b17f
jsloan3 <jaxon.sloan@ucalgary.ca>
2025-03-20 14:45:08 -0600

    god bless it works

---
+++ server/client/client.py
@@ -0,0 +1,191 @@
+import os
+import hmac
+import hashlib
+from pprint import pprint, pformat
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+import dr
+import requests
+import json
+
+HOSTNAME = "http://127.0.0.1:5000"
+
+class Person:
+    def __init__(self, phone: str):
+        self.phone = phone
+
+class Contacts:
+    def __init__(self):
+        self.contacts = []
+    def add_contact(self, contact : Person):
+        self.contacts.append(contact)
+
+class Client:
+    def __init__(self, name, phone):
+        self.name = name
+        self.phone = phone
+        self.ratchets = []
+        self.dh_keys = []
+    
+    def add_ratchet(self, ratchet : dr.DoubleRatchet, person : Person):
+        self.ratchets.append({"person":person,"ratchet":ratchet})
+
+    def get_pub_key(self, person : Person):
+        to_send = {"phone" : person.phone}
+        resp = requests.post(f"{HOSTNAME}/get_pub_key", json=to_send).json()
+        return resp
+
+    def add_pub_key_serv(self, keytuple):
+        pubkey = keytuple[0]
+        privkey = keytuple[1]
+        id = keytuple[2]
+        self.dh_keys.append({"pubkey":pubkey,"privkey":privkey,"key_id":id})
+        to_send = {"sender":self.phone,"pubkey":pubkey,"key_id":id}
+        resp = requests.post(f"{HOSTNAME}/add_pub_key", json=to_send)
+
+        return resp.ok
+    
+    def add_pub_key_local(self, keytuple):
+        pubkey = keytuple[0]
+        privkey = keytuple[1]
+        id = keytuple[2]
+        self.dh_keys.append({"pubkey":pubkey,"privkey":privkey,"key_id":id})
+
+    def make_keys(self):
+        privkey = x25519.X25519PrivateKey.generate()
+        pubkey = privkey.public_key().public_bytes_raw().hex()
+        privkey = privkey.private_bytes_raw().hex()
+        identifier = os.urandom(32).hex()
+
+        return (pubkey, privkey, identifier)
+    
+    def get_shared_key(self):
+        data = {"hi":"hello"}
+        resp = requests.post(f"{HOSTNAME}/get_shared_key", json=data).json()
+        return resp["shared_key"]
+    
+    def find_priv_from_id(self, id):
+        print(f"DH KEYS: {self.dh_keys}")
+        for d in self.dh_keys:
+            if d["key_id"] == id:
+                print(f"found KEY!!!: {d["key_id"]}")
+                return d["privkey"]
+        return None
+    
+    def send_first_message(self, phone, message):
+        shared_key = bytes.fromhex(self.get_shared_key())
+        my_pubkey_str, my_privkey_str, my_key_id = self.make_keys()
+        
+        keybundle = self.get_pub_key(Person(phone))
+        their_key_id : str = keybundle["key_id"]
+        their_pub_key = x25519.X25519PublicKey.from_public_bytes(bytes.fromhex(keybundle["pubkey"]))
+        my_privkey = x25519.X25519PrivateKey.from_private_bytes(bytes.fromhex(my_privkey_str))
+
+    
+        ratch, dh_ex = self.make_ratchet_as_sender(shared_key=shared_key, our_private_key=my_privkey, their_public=their_pub_key)
+        out = ratch.encrypt_message(message)
+    
+
+        to_send = {"sender":self.phone,"recipient":phone,
+                     "message":out,"sender_pub":my_pubkey_str,
+                     "rec_key_id":their_key_id,"dh_key":dh_ex.hex()}
+        
+        data = json.dumps(to_send, default=self.cust_serializer)
+        data = json.loads(data)
+        print(data)
+        resp = requests.post(f"{HOSTNAME}/send_message", json=data)
+
+    
+    def cust_serializer(self, obj):
+        if isinstance(obj, bytes):
+            return obj.hex()
+        raise TypeError(f"Type {type(obj)} not serializable.")
+    
+    def get_oldest_msg_from_num(self, target):
+        to_send = {"requester" : self.phone, "target" : target}
+        res = requests.post(f"{HOSTNAME}/get_oldest_msg_from_num", json=to_send)
+        return res.json()
+    
+    def decode_first_message(self, message):
+        print(f"yapdollar!!!!: {message}")
+        message = self.json_to_dr_format(message)
+        message = self.recursive_json(message)
+        shared_key = bytes.fromhex(self.get_shared_key())
+        their_pub_key = bytes.fromhex(message["sender_pub"])
+        their_pub_key = x25519.X25519PublicKey.from_public_bytes(their_pub_key)
+        their_dh_key = bytes.fromhex(message["dh_key"])
+        our_private_key = bytes.fromhex(self.find_priv_from_id(message["rec_key_id"]))
+        our_private_key = x25519.X25519PrivateKey.from_private_bytes(our_private_key)
+
+        ratch = self.make_ratchet_as_reciever(shared_key=shared_key, our_private_key=our_private_key,
+                                      their_public=their_pub_key, their_dh=their_dh_key)
+        
+        decode = ratch.decrypt_message(message["msg"])
+        print(decode)
+        
+
+    def json_to_dr_format(self, message):
+        message['msg']['nonce'] = bytes.fromhex(message['msg']['nonce'])
+        message['msg']['ciphertext'] = bytes.fromhex(message['msg']['ciphertext'])
+        return message
+    
+    def recursive_json(self, data):
+        if isinstance(data, str):
+            try:
+                parsed = json.loads(data)
+                return self.recursive_json(parsed)
+            except:
+                return data
+        elif isinstance(data, dict):
+            return {key: self.recursive_json(value) for key, value in data.items()}
+        elif isinstance(data, list):
+            return [self.recursive_json(item) for item in data]
+        return data
+
+
+    def make_ratchet_as_sender(self, shared_key: bytes, our_private_key: x25519.X25519PrivateKey, 
+                               their_public: x25519.X25519PublicKey):
+        our_exchange = our_private_key.exchange(their_public)
+        our_send, our_recv = dr.initial_key_derivation(shared_key, our_exchange)
+
+        our_ratchet = dr.DoubleRatchet(
+            root_key = shared_key,
+            dh_pair = our_private_key,
+            remote_dh_public=their_public,
+            sending_chain_key=our_send,
+            receiving_chain_key=our_recv
+        )
+        return (our_ratchet, our_exchange)
+
+    def make_ratchet_as_reciever(self, shared_key: bytes, our_private_key: x25519.X25519PrivateKey, 
+                               their_public: x25519.X25519PublicKey, their_dh: bytes):
+        our_recv, our_send = dr.initial_key_derivation(shared_key, their_dh)
+
+        our_ratchet = dr.DoubleRatchet(
+            root_key = shared_key,
+            dh_pair = our_private_key,
+            remote_dh_public=their_public,
+            sending_chain_key=our_send,
+            receiving_chain_key=our_recv
+        )
+        return our_ratchet
+
+def main():
+    
+    bob = Client("bob", "1231231234")
+    alice = Client("alice", "3213213210")
+
+    bob.add_pub_key_serv(bob.make_keys())
+    alice.send_first_message('1231231234', 'sauce')
+
+    x = bob.get_oldest_msg_from_num('3213213210')
+
+    bob.decode_first_message(x)
+
+if __name__ == '__main__':
+    main()
--- server/main.py
+++ server/main.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from seed import User, Message, Pubkey, create_db
 from json import *
 import json
+
 app = Flask(__name__)
 
 if not os.path.isfile("main.db"):
@@ -17,166 +18,94 @@ engine = create_engine("sqlite:///main.db", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+messages = []
+pub_keys = []
+sharedkey = os.urandom(32).hex()
+
 @app.route('/')
 def index():
     return "Hello!"
 
-@app.route("/register", methods=['POST'])
-def register():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    
-    sha256 = hashlib.sha256()
-    sha256.update(input.get("PhoneNum").encode())
-    
-    phone = sha256.hexdigest()
-    name = input.get("Name")
-    public = input.get("EdPublic")
-
-    print(phone, name, public)
-
-    try:
-        session.add(User(name=name, phonehash=phone, edpublic=public))
-        session.commit()
-    except Exception as e:
-        raise e
-        return "Database write error", 400
-
-    return "Registered!", 200
-
-@app.route("/user_from_phone", methods=['POST'])
-def user_from_phone():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    phone = input.get("phone").encode()
-    sha256 = hashlib.sha256()
-    sha256.update(phone)
-    phonehash = sha256.hexdigest()
-
-    user = session.query(User).filter(User.phonehash == phonehash).first()
-    if user:
-        return user.name, 200
-    return "User not found.", 400
-
-@app.route("/ed_from_phone", methods=['POST'])
-def ed_from_phone():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    phone = input.get("phone").encode()
-    sha256 = hashlib.sha256()
-    sha256.update(phone)
-    phonehash = sha256.hexdigest()
-
-    user = session.query(User).filter(User.phonehash == phonehash).first()
-    if user:
-        return user.edpublic, 200
-    return "User not found.", 400
-
-@app.route("/get_pub_from_phone", methods=['POST'])
-def pub_from_phone():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    phone = input.get("phone").encode()
-    sha256 = hashlib.sha256()
-    sha256.update(phone)
-    phonehash = sha256.hexdigest()
-    user = session.query(User).filter(User.phonehash == phonehash).first()
-    if not user:
-        return "no user found", 400
-    pub = session.query(Pubkey).filter(Pubkey.owner == user.edpublic).first()
-    if not pub:
-        return "user out of pubkeys, try again later", 400
-    return pub.keycontent, 200
-
-@app.route("/user_from_ed", methods=['POST'])
-def user_from_ed():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    edpub = input.get("edpub").encode()
-
-    user = session.query(User).filter(User.edpublic == edpub).first()
-    if user:
-        return user.name, 200
-    return "User not found.", 400
-
-## TODO: Instead of just taking the public ed, take a signature and validate using the stored ed
-@app.route("/retrieve_messages", methods=['POST'])
-def retrieve_messages():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    user_ed = input.get("EdPublic").encode()
-    user_id = session.query(User).filter(User.edpublic == user_ed).first()
-    if not user_id:
-        return "User not found", 400
-    all_messages = session.query(Message).filter(Message.recipient == user_id).all()
-    if not all_messages or all_messages == []:
-        return "No new messages", 400
-    msg_dict = [msg.to_dict() for msg in all_messages]
-    return jsonify(msg_dict), 200
-
 @app.route("/send_message", methods=['POST'])
 def send_message():
+    global messages, pub_keys
+    bundle = request.json
 
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    phone = input.get("phone").encode()
-    sha256 = hashlib.sha256()
-    sha256.update(phone)
-    phonehash = sha256.hexdigest()
-    edpub = input.get("edpub").encode()
-    data = input.get("data").encode()
-    sender_pub = input.get("sender_pub")
-    receiver_pub = input.get("receiver_pub")
-    receiver_id = session.query(User).filter(User.phonehash == phonehash).first().id
-    sender_id = session.query(User).filter(User.edpublic == edpub).first().id
-    session.add(Message(sender=sender_id,recipient=receiver_id,data=data,sender_pub=sender_pub, recipient_pub=receiver_pub))
-    session.commit()
+    sender = bundle["sender"]
+    recipient = bundle["recipient"]
+    msg = bundle["message"]
+    sender_pub = bundle["sender_pub"]
+    rec_key_id = bundle["rec_key_id"]
+    dh_key = bundle["dh_key"]
 
-    return "message sent", 200
-
-    pass
-
-@app.route("/add_pubkey", methods=['POST'])
-def add_pubkey():
-    input = request.get_json()
-    if not input:
-        return "Bad JSON", 400
-    edpub = input.get("edpub")
-    keycontent = input.get("keycontent")
-    print(edpub)
+    messages.append({"sender":sender,"recipient":recipient,
+                     "msg":msg,"sender_pub":sender_pub,
+                     "rec_key_id":rec_key_id,"dh_key":dh_key})
     
-    session.add(Pubkey(owner=edpub, keycontent=keycontent))
-    session.commit()
+    print(f"MESSAGES: {messages}")
 
-    return "complete", 200
+    return jsonify({"status":"sent"}), 200
 
+@app.route("/receive_messages", methods=['POST'])
+def receive_messages():
+    global messages, pub_keys
+    bundle = request.json
 
+    response = []
 
-def get_user_from_phone(phone):
-    sha256 = hashlib.sha256()
-    sha256.update(phone)
-    phonehash = sha256.hexdigest()
-    
-    res = session.query(User).filter(User.phonehash == phonehash).first()
-    if not res:
-        return None
-    return res
+    requester = bundle["requester"]
 
-    
+    for m in messages:
+        if m["recipient"] == requester:
+            response.append(m)
 
+    return jsonify(response), 200
 
-## TODO: DEBUG METHOD, REPLACE WITH PROPER KEY EXCHANGE
+@app.route("/add_pub_key", methods=['POST'])
+def add_pub_key():
+    global pub_keys
+    bundle = request.json
+    sender = bundle["sender"]
+    pubkey = bundle["pubkey"]
+    identifier = bundle["key_id"]
+
+    pub_keys.append({"sender": sender, "pubkey" : pubkey, "key_id": identifier})
+
+    return jsonify({"status":"added"}), 200
+
+@app.route("/get_pub_key", methods=['POST'])
+def get_pub_key():
+    global pub_keys
+    bundle = request.json
+    phone = bundle["phone"]
+
+    first = None
+    i = 0
+    for key in pub_keys:
+        if key["sender"] == phone:
+            first = key
+            # pub_keys.pop(i)
+            break
+    if first == None:
+        return jsonify({"status":"couldn't find pubkey"}), 400
+    return jsonify(first), 200
+
+@app.route("/get_oldest_msg_from_num", methods=['POST'])
+def get_oldest_msg_from_num():
+    global messages
+    bundle = request.json
+    requester = bundle["requester"]
+    target = bundle["target"]
+    for m in messages:
+        if m["recipient"] == requester and m["sender"] == target:
+            return jsonify(m), 200
+    return "could not find number", 400
+
+## TODO: make actual key sharing, this is a debug method
 @app.route("/get_shared_key", methods=['POST'])
 def get_shared_key():
-    key = '95a36d514a26b653e863441c18f78f64ac76aa6741126c3a2feadd85309c6132'
-    return key, 200
+    global sharedkey
+    return jsonify({"shared_key": sharedkey}), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
