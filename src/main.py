220e4d73eb4c07a6c0387a389f58ee23efee4642
Sidd <sidd.s.pai@gmail.com>
2025-03-23 16:19:43 -0600

    Added comment for later changes.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main() -> str:
-    return render_template("layout.html")
+    return render_template("layout.html")  # Change this to the actual html file.
 
 
 if __name__ == "__main__":
