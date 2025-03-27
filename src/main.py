3dd0cdb989636a1e1f723302e3b012836759de9e
George Vassilev <vassilevgeorge7@gmail.com>
2025-03-27 13:06:03 -0600

    added Contacts page

--- src/main.py
+++ src/main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def main() -> str:
     return render_template("layout.html")  # Change this to the actual html file.
 
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
