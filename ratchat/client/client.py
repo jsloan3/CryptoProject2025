30cda23a5128a08ce653a3f82ca423c00c52128f
jsloan3 <jaxon.sloan@ucalgary.ca>
2025-04-09 04:15:45 -0600

    implements double ratchet into the new frontend

--- ratchat/client/client.py
+++ ratchat/client/client.py
@@ -1,12 +1,21 @@
-import datetime
-import json
-import requests, time, sys
+import requests, time, sys, random, uuid, os, json, datetime, argparse
 from flask import Flask, redirect, render_template, request
-import argparse
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import double_ratchet.dr
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 SERVER_HOST = 'http://127.0.0.1:8000'
 USER_DATA_FILE = 'user_data.json'
 MESSAGE_STORAGE = 'message_storage.json'
+
+global_pre_keys = []
+
+global_ratchets = []
+
 my_phonenum = None
 
 app = Flask(__name__)
@@ -47,6 +56,8 @@ def set_phone_num():
     my_phonenum = res
 
     save_user_data()
+
+    make_prekeys(5)
     return redirect('/')
 
 @app.route('/add_new_contact', methods=['GET'])
@@ -58,6 +69,7 @@ def add_new_contact():
 def send_new_contact():
     recipient = request.form.get('phone_num')
     message = request.form.get('contact_message')
+    prekey_id, my_pubkey = make_new_ratchet_as_sender(recipient)
     send_message(recipient, message)
     return redirect(f'/contacts')
 
@@ -70,63 +82,134 @@ def index():
     else:
         return redirect('/contacts')
 
+def make_new_ratchet_as_sender(receiver : str):
+    global global_ratchets
+    data = {"receiver": receiver}
+    rec_pre_key_json = requests.post(f"{SERVER_HOST}/get_pre_key", json=data).json()
+    rec_pre_key = rec_pre_key_json['pub_pre']
+    prekey_id = rec_pre_key_json['identifier']
+    my_privkey = x25519.X25519PrivateKey.generate()
+    my_pubkey = my_privkey.public_key().public_bytes_raw().hex()
+    xkey = x25519.X25519PublicKey.from_public_bytes(bytes.fromhex(rec_pre_key))
+    shared_key = my_privkey.exchange(xkey)
+
+    my_send, my_receive = double_ratchet.dr.derive_keys(shared_key, shared_key)
+
+    ratchet = double_ratchet.dr.DoubleRatchet(
+        root_key = shared_key,
+        dh_pair = my_privkey,
+        remote_dh_public=xkey,
+        sending_chain_key=my_send,
+        receiving_chain_key=my_receive
+    )
+    print(rec_pre_key)
+
+    global_ratchets += [{'contact': receiver, 'ratchet': ratchet, 'prekey_id': prekey_id, 'starter_pubkey': my_pubkey}]
+
+    print(global_ratchets)
+
+    return prekey_id, my_pubkey
+
+def make_new_ratchet_as_receiver(sender, prekey_id, remote_pubkey):
+    global global_ratchets
+
+    priv_pre = get_prekey_from_global(prekey_id)
+    privkey = x25519.X25519PrivateKey.from_private_bytes(bytes.fromhex(priv_pre))
+    their_pubkey = x25519.X25519PublicKey.from_public_bytes(bytes.fromhex(remote_pubkey))
+    shared_key = privkey.exchange(their_pubkey)
+    my_receive, my_send = double_ratchet.dr.derive_keys(shared_key, shared_key)
+
+    ratchet = double_ratchet.dr.DoubleRatchet(
+        root_key = shared_key,
+        dh_pair = privkey,
+        remote_dh_public = their_pubkey,
+        sending_chain_key= my_send,
+        receiving_chain_key= my_receive
+    )
+
+    global_ratchets += [{'contact': sender, 'ratchet': ratchet, 'prekey_id': prekey_id, 'starter_pubkey': remote_pubkey}]
+    print(global_ratchets)
+    return 
+
+def make_prekeys(n : int):
+    global global_pre_keys
+    for i in range(n):
+        priv_pre = x25519.X25519PrivateKey.generate()
+        pub_pre = priv_pre.public_key()
+
+        pub_pre_raw = pub_pre.public_bytes_raw().hex()
+        priv_pre_raw = priv_pre.private_bytes_raw().hex()
+        key_identifier = str(uuid.uuid4())
+        data = {'pub_pre': pub_pre_raw, 'id': key_identifier, 'receiver':my_phonenum}
+        requests.post(f"{SERVER_HOST}/add_pre_key", json=data)
+
+        global_pre_keys += [{'pub_pre': pub_pre_raw, 'priv_pre': priv_pre_raw, 'id': key_identifier}]
+
+    print(global_pre_keys)
 
 def check_messages(phonenum):
     try:
         res = requests.get(f"{SERVER_HOST}/receive/{phonenum}")
         res.raise_for_status()
-
-        messages = res.json()
-        if messages:
-            print("---You got mail!---")
-            for m in messages:
-                print(f"From {m['sender']} ({m['timestamp']}): {m['message']}")
-            print("-------------------")
-            save_messages(messages)
-            return messages
-
     except:
         print("Error retriving messages occured.")
+        return
+
+    messages = res.json()
+    if messages:
+        print("---You got mail!---")
+        for m in messages:
+            print(f"From {m['sender']} ({m['timestamp']}): {m['message']}")
+        print("-------------------")
+
+        for m in messages:
+            sender_num = m['sender']
+            sender_ratchet = check_for_ratchet(sender_num)
+            if sender_ratchet == None:
+                make_new_ratchet_as_receiver(m['sender'], m['prekey_id'], m['starter_pubkey'])
+                sender_ratchet: double_ratchet.dr.DoubleRatchet = check_for_ratchet(sender_num)['ratchet']
+            else:
+                sender_ratchet = sender_ratchet['ratchet']
+            decmessage = sender_ratchet.decrypt_message(m['message'])
+            m['message'] = decmessage
+
+        save_messages(messages)
+        return messages
+
+    
 
 def send_message(phonenum, message):
     timestamp = datetime.datetime.now().isoformat()
+    ratchet = check_for_ratchet(phonenum)
+    dratchet: double_ratchet.dr.DoubleRatchet = ratchet['ratchet']
+    ciphertext = dratchet.encrypt_message(message)
+    print(ciphertext)
     to_send = {
         'sender': my_phonenum,
         'recipient': phonenum,
-        'message': message,
+        'message': ciphertext,
         'timestamp': timestamp,
         'own_message': False,
+        'prekey_id': ratchet['prekey_id'],
+        'starter_pubkey': ratchet['starter_pubkey']
     }
+    to_save = to_send.copy()
+    to_save['message'] = message
     try:
         res = requests.post(f"{SERVER_HOST}/send", json=to_send)
         if not res.ok:
             raise Exception
-        save_own_message(to_send)
+        save_own_message(to_save)
     except Exception as e:
         print(f"An error occured while sending the message: {e}")
 
-def main():
-    global my_phonenum
-    my_phonenum = input("Enter your phone #: ")
-    print(f"You are now registered under phone #: {my_phonenum}")
+def check_for_ratchet(phonenum):
 
-    while True:
-        choice = input("Type 'c' to check messages. Type 's' to send a message. Type 'q' to quit: ")
-        match choice:
-            case 'c':
-                messages = check_messages(my_phonenum)
-            case 's':
-                dest = input("What # to send to?: ")
-                mes = input("Message: ")
-                send_message(dest, mes)
-            case 'q':
-                print("Goodbye. Exiting.")
-                exit()
-            case 'p':
-                num = input("What phone# to scan for?: ")
-                print(get_phone_messages(num))
-            case _:
-                print("Invalid choice. Try again.")
+    for r in global_ratchets:
+        if phonenum == r['contact']:
+            return r
+    return None
+
 
 def load_user_data():
     global my_phonenum
@@ -183,7 +266,9 @@ def save_own_message(new_message):
         'recipient': new_message['sender'],
         'message': new_message['message'],
         'timestamp': new_message['timestamp'],
-        'own_message': True
+        'own_message': True,
+        'prekey_id': new_message['prekey_id'],
+        'starter_pubkey': new_message['starter_pubkey']
     }
     save_messages([to_save])
 
@@ -206,6 +291,11 @@ def get_users_from_messages(messages):
             contacts.append(m['sender'])
     return contacts
 
+def get_prekey_from_global(prekey_id):
+    for p in global_pre_keys:
+        if prekey_id == p['id']:
+            return p['priv_pre']
+    return None
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ratchat")
--- ratchat/server/server.py
+++ ratchat/server/server.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # queue structure: { 'username': [ {'sender': 'sender_name', 'message': 'content', 'timestamp': 'isoformat_time'}, ... ] }
 message_queue = defaultdict(list)
+pubkeys = []
 
 @app.route('/send', methods=['POST'])
 def send_message():
@@ -16,13 +17,17 @@ def send_message():
     recipient = data['recipient']
     message = data['message']
     timestamp = data['timestamp']
+    prekey_id = data['prekey_id']
+    starter_pubkey = data['starter_pubkey']
 
     message_data = {
         'sender': sender,
         'recipient': recipient,
         'message': message,
         'timestamp': timestamp,
-        'own_message': False
+        'own_message': False,
+        'prekey_id': prekey_id,
+        'starter_pubkey': starter_pubkey
     }
 
     message_queue[recipient].append(message_data)
@@ -37,6 +42,27 @@ def recieve_message(phonenum):
         message_queue[phonenum] = []
 
     return jsonify(messages), 200
+
+@app.route("/add_pre_key", methods=['POST'])
+def add_pre_key():
+    global pubkeys
+    json = request.get_json()
+    identifier = json["id"]
+    prekey = json["pub_pre"]
+    receiver = json["receiver"]
+    pubkeys.append({"identifier":identifier, "pub_pre":prekey, "receiver":receiver})
+
+    print(f"Stored prekey {prekey} for user {receiver} with id {identifier}.")
+
+    return "Added Prekey", 200
+
+@app.route("/get_pre_key", methods=['POST'])
+def get_pre_key():
+    json = request.get_json()
+    to_get = json["receiver"]
+    for k in pubkeys:
+        if to_get == k["receiver"]:
+            return k, 200
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=8000, debug=True, threaded=False)
